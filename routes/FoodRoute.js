823e4da9a0b495e59944cfca99fa3e142801c90a
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2023-06-04 15:03:20 +0530

    made protected route for posting

--- routes/FoodRoute.js
+++ routes/FoodRoute.js
@@ -7,8 +7,7 @@ const router = express.Router();
 router
   .route('/')
   .get(authController.protect, FoodController.getOrder) //not making protected route for now..
-  .get(FoodController.getOrder)
-  .post(FoodController.createOrder);
+  .post(authController.protect, FoodController.createOrder);
 
 router
   .route('/:id')
