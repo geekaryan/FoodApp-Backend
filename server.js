62f57eb77c54be396eb058290131e12cd0a102ba
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2024-11-13 11:34:52 +0530

    added ignore file

--- server.js
+++ server.js
@@ -3,18 +3,19 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = `mongodb+srv://tester1:1234@cluster0.zv8kinz.mongodb.net/FoodApp?retryWrites=true&w=majority`;
-
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
 mongoose
   .connect(DB, {
+    useCreateIndex: true,
+    useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
     console.log('DATABASE is connected');
-  })
-  .catch((err) => {
-    console.error('Connection error', err);
   });
 
 const port = 7000;
