f3153513a04f9b0c6735c945966cb21badc004a8
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2024-11-19 10:21:33 +0530

    19-11-24 added the previous push i have made

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,43 +16,6 @@ exports.findAll = async (req, res, next) => {
   }
 };
 
-// exports.findOne = async (req, res, next) => {
-//   try {
-//     const userId = req.params.id;
-
-//     const orders = await User.aggregate([
-//       {
-//         $match: { _id: new mongoose.Types.ObjectId(userId) },
-//       },
-//       {
-//         $lookup: {
-//           from: 'placeds',
-//           localField: '_id',
-//           foreignField: 'customer_id',
-//           as: 'orders',
-//         },
-//       },
-//     ]);
-
-//     if (!orders.length) {
-//       return res.status(404).json({
-//         status: 'fail',
-//         message: 'No user found with the provided ID',
-//       });
-//     }
-
-//     res.status(200).json({
-//       status: 'success',
-//       data: orders[0],
-//     });
-//   } catch (err) {
-//     res.status(500).json({
-//       status: 'fail',
-//       message: err.message,
-//     });
-//   }
-// };
-
 exports.findOne = async (req, res, next) => {
   try {
     const userId = req.params.id;
