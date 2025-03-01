870df6d81317fbe71a87901c46fad31ea60b800e
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2025-03-02 00:24:06 +0530

    focus on building rating system

--- modal/Menumodal.js
+++ modal/Menumodal.js
@@ -10,9 +10,16 @@ const menuModel = new mongoose.Schema({
     type: String,
     required: [true, 'An item must have a tast description'],
   },
-  ratings: {
+  ratingsAverage: {
     type: Number,
-    required: [true, 'Ratings of food items must be there'],
+    default: 4.5,
+    min: [1, 'A rating must be above 1.0'],
+    max: [5, 'A rating must be below 5.0'],
+    set: (val) => Math.round(val * 10) / 10, //this is used to round off values
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   tags: {
     type: [],
